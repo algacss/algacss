a756bbde94297a0ba44355a661b157e2a0db5fdb
Tedir Ghazali <tedirghazali@gmail.com>
2022-10-30 14:31:05 +0700

    improved container

--- test/layouts/container.test.js
+++ test/layouts/container.test.js
@@ -5,67 +5,77 @@ test('Testing container component', async () => {
     size: empty;
   }`
   const output = `.container {
-    width: auto;
+    width: 100%;
+    max-width: auto;
     margin-left: 0.375rem;
     margin-right: 0.375rem
 }
 .containerFluid {
-    width: auto;
+    width: 100%;
+    max-width: auto;
     margin-left: 0.375rem;
     margin-right: 0.375rem
 }
 @media (min-width: 600px) {
     .container {
-        width: 576px;
+        width: 100%;
+        max-width: 576px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 768px) {
     .container {
-        width: 720px;
+        width: 100%;
+        max-width: 720px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 1024px) {
     .container {
-        width: 864px;
+        width: 100%;
+        max-width: 864px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 1280px) {
     .container {
-        width: 1080px;
+        width: 100%;
+        max-width: 1080px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 1536px) {
     .container {
-        width: 1200px;
+        width: 100%;
+        max-width: 1200px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 1920px) {
     .container {
-        width: 1440px;
+        width: 100%;
+        max-width: 1440px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 2560px) {
     .container {
-        width: 2048px;
+        width: 100%;
+        max-width: 2048px;
         margin-left: auto;
         margin-right: auto
     }
 }
 @media (min-width: 3840px) {
     .container {
-        width: 3440px;
+        width: 100%;
+        max-width: 3440px;
         margin-left: auto;
         margin-right: auto
     }
